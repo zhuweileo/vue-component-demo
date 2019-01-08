c0a39b66217b7763d43f565ab371f040e5e62a68
zhuwei <zhuwei_leo@163.com>
2019-01-08 21:54:03 +0800

    编写测试用例

--- test/button.spec.js
+++ test/button.spec.js
@@ -1,12 +1,35 @@
 import MButton from '../src/MButton'
 import {mount} from "@vue/test-utils";
+import Syn from 'syn'
 
 describe('MButton.vue',function () {
 
   it('can set type prop',function () {
-    const wrapper = mount(MButton);
+    const wrapper = mount(MButton,{
+      propsData:{
+        type: 'warn',
+      }
+    });
     const vm = wrapper.vm;
     expect(vm.$el.classList.contains('warn')).to.be.true
+  })
+
+  it('can click',function (done) {
+    const click = sinon.spy();
+    const haha = sinon.spy();
+    const wrapper = mount(MButton,{
+      propsData:{
+        type: 'warn',
+      },
+      listeners:{
+        click,
+      }
+    });
+
+    Syn.click(wrapper.vm.$el,function () {
+      sinon.assert.calledWith(click);
+      done();
+    });
   })
 
 });
